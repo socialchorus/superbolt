90cd028c7dd496046d398c34c27baf2b68f3052c
Sumeet Agarwal <sumeet.agarwal@socialchorus.com>
2014-10-01 23:00:04 +0000

    Fix Runner::Pg to reconnect bad PG connections.
    The fix was originally introduced in c842b4f8ef8d963b4c1ee7356ada4b9aa7164f0a,
    but didn't work.

--- lib/superbolt/runner/pg.rb
+++ lib/superbolt/runner/pg.rb
@@ -2,11 +2,19 @@ module Superbolt
   module Runner
     class Pg < AckOne
       def on_error(message, error)
-        if error == PG::UnableToSend || error == PG::ConnectionBad
-          ActiveRecord::Base.connection.reset!
+        if reconnect_after_error?(error)
+          ActiveRecord::Base.connection.reconnect!
         end
 
         super
+      end
+
+      private
+
+      def reconnect_after_error?(error)
+        error.is_a?(ActiveRecord::StatementInvalid) and
+          error.message.start_with?("PG::UnableToSend") or
+          error.message.start_with?("PG::ConnectionBad")
       end
     end
   end
--- spec/lib/superbolt/runner/pg_spec.rb
+++ spec/lib/superbolt/runner/pg_spec.rb
@@ -2,52 +2,45 @@ require 'spec_helper'
 
 module ActiveRecord
   class Base
+    def self.connection
+      @@connection
+    end
+
+    @@connection = Class.new do
+      def self.reset!
+      end
+    end
+  end
+
+  class StatementInvalid < StandardError
   end
 end
 
-module PG
-  class UnableToSend
-    def self.message
-      'oh noes'
-    end
+PG_UNABLE_TO_SEND_ERROR = ActiveRecord::StatementInvalid.new("PG::UnableToSend: server closed the connection unexpectedly\n\tThis probably means the server terminated abnormally\n\tbefore or while processing the request.\n: SELECT  \"data_sources\".* FROM \"data_sources\"  WHERE \"data_sources\".\"program_id\" = 1 AND \"data_sources\".\"identifier\" = 'qz.com/feed'  ORDER BY \"data_sources\".\"id\" ASC LIMIT 1")
 
-    def self.backtrace
-      'trace me'
-    end
-  end
-
-  class ConnectionBad
-    def self.message
-      'oh noes'
-    end
-
-    def self.backtrace
-      'trace me'
-    end
-  end
-end
+PG_CONNECTION_BAD_ERROR = ActiveRecord::StatementInvalid.new("PG::ConnectionBad: PQsocket() can't get socket descriptor: BEGIN")
 
 describe "Superbolt::Runner::Pg" do
-  let(:runner) { Superbolt::Runner::Pg.new('berkin', Superbolt::ErrorNotifier::None.new, 'whatever', 'some block') }
-  let(:connection) { double('connection', reset!: true) }
+  subject(:runner) { Superbolt::Runner::Pg.new('berkin', Superbolt::ErrorNotifier::None.new, 'whatever', 'some block') }
 
-  before do
-    ActiveRecord::Base.stub(:connection).and_return(connection)
+  describe "#on_error" do
+    it "reconnects ActiveRecord when it gets PG::UnableToSend" do
+      expect(ActiveRecord::Base.connection).to receive(:reconnect!)
+      report_error(PG_UNABLE_TO_SEND_ERROR)
+    end
+
+    it "reconnects ActiveRecord when it gets PG::ConnectionBad" do
+      expect(ActiveRecord::Base.connection).to receive(:reconnect!)
+      report_error(PG_CONNECTION_BAD_ERROR)
+    end
+
+    it "doesn't reconnect for AR::StatementInvalid with a different message" do
+      expect(ActiveRecord::Base.connection).to_not receive(:reconnect!)
+      report_error(ActiveRecord::StatementInvalid.new("some other message"))
+    end
   end
 
-  describe "on_error" do
-    context "PG::UnableToSend" do
-      it "should handle it" do
-        connection.should_receive(:reset!).and_return(true)
-        runner.on_error({message: 'somethig has gone very wrong'}, PG::UnableToSend)
-      end
-    end
-
-    context "PG::ConnectionBad " do
-      it "should handle it" do
-        connection.should_receive(:reset!).and_return(true)
-        runner.on_error({message: 'somethig has gone very wrong'}, PG::ConnectionBad)
-      end
-    end
+  def report_error(exception)
+    runner.on_error({"superbolt_message_payload" => "hello"}, exception)
   end
 end
